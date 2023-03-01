497a87ab8d883be6d1cd1da1b0724160eebcc0ab
giorgi568 <mateshvili02@mail.ru>
2023-03-01 04:03:46 +0400

    user input done, polishing left

--- script.js
+++ script.js
@@ -19,36 +19,63 @@ function addBookToLibrary(title, author, pages, readOrNot) {
   myLibrary.push(new book(title, author, pages, readOrNot));
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'read');
-addBookToLibrary('blood meridian', 'cormac mcCarthy', 280, 'notread');
+// addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'read');
+// addBookToLibrary('blood meridian', 'cormac mcCarthy', 280, 'notread');
+
 // console.log(myLibrary);
 // console.log(myLibrary[0]);
 
 const bookShelf = document.querySelector(".bookShelf");
 // console.log(bookShelf);
 
-function displayBooks() {
+function displayBooks(i) {
   
-  for(let i=0; i<myLibrary.length; i++){
-    const trTag = document.createElement("tr");
-    const tdTag1 = document.createElement("td");
-    const tdTag2 = document.createElement("td");
-    const tdTag3 = document.createElement("td");
-    const tdTag4 = document.createElement("td");
+  // for(let i=0; i<myLibrary.length; i++){
+  const trTag = document.createElement("tr");
+  const tdTag1 = document.createElement("td");
+  const tdTag2 = document.createElement("td");
+  const tdTag3 = document.createElement("td");
+  const tdTag4 = document.createElement("td");
     
-    bookShelf.appendChild(trTag);
+  bookShelf.appendChild(trTag);
     
-    tdTag1.textContent = `${myLibrary[i].title}`
-    tdTag2.textContent = `${myLibrary[i].author}`
-    tdTag3.textContent = `${myLibrary[i].pages}`
-    tdTag4.textContent = `${myLibrary[i].readOrNot}`
+  tdTag1.textContent = `${myLibrary[i].title}`
+  tdTag2.textContent = `${myLibrary[i].author}`
+  tdTag3.textContent = `${myLibrary[i].pages}`
+  tdTag4.textContent = `${myLibrary[i].readOrNot}`
   
-    trTag.appendChild(tdTag1);
-    trTag.appendChild(tdTag2);
-    trTag.appendChild(tdTag3);
-    trTag.appendChild(tdTag4);
-  }
+  trTag.appendChild(tdTag1);
+  trTag.appendChild(tdTag2);
+  trTag.appendChild(tdTag3);
+  trTag.appendChild(tdTag4);
+  // }
 
 }
 
 // displayBooks();
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  // console.log(document.querySelector("#title").value)
+  const titleUser = document.querySelector("#title");
+  const authorUser = document.querySelector("#author");
+  const pagesUser = document.querySelector("#pages");
+  const readOrNotUser = document.querySelector("#readOrNot");
+  addBookToLibrary(titleUser.value, authorUser.value, pagesUser.value, readOrNotUser.value);
+
+  titleUser.value = '';
+  authorUser.value = '';
+  pagesUser.value = '';
+  readOrNotUser.value = '';
+
+  // console.log(myLibrary);
+  displayBooks(myLibrary.length-1);
+})
+
+const form = document.querySelector("#add-book");
+const btnAdd = document.querySelector("#addNewBook");
+btnAdd.addEventListener("click", (e) => {
+  form.classList.add("active");
+
+})
